650fe7c6cde2748b39b061f23e1d5468d776b0dc
bkhartmire <bkhartmire@gmail.com>
2018-01-13 17:12:53 +0100

    more restaurant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 cuisines = Cuisine.create([{name: 'Japanese'}, {name: 'Chinese'}, {name: 'Korean'}, {name: 'Italian'}, {name: 'French'}, {name: 'New American'}, {name: 'Mexican'}, {name: 'Fast Food'}, {name: 'Tapas'}])
 
-cities = City.create([{name: 'Los Angeles'}, {name: 'New York City'}, {name: 'Barcelona'}, {name: 'Paris'}, {name: 'Tokyo'}, {name: 'Hong Kong'}, {name: 'Mexico City'}, {name: 'Cape Town'}, {name: 'Melbourne'}])
+cities = City.create([{name: 'Los Angeles'}, {name: 'New York City'}, {name: 'Barcelona'}, {name: 'Paris'}, {name: 'Tokyo'}, {name: 'Hong Kong'}, {name: 'Mexico City'}, {name: 'Cape Town'}])
 
 restaurants = Restaurant.create([
     {name: 'Cal Mare', city_id: City.find_by(name: 'Los Angeles').id , cuisine_id: Cuisine.find_by(name: 'Italian').id},
@@ -24,5 +24,29 @@ restaurants = Restaurant.create([
     {name: 'Floreta', city_id: City.find_by(name: 'Barcelona').id , cuisine_id: Cuisine.find_by(name: 'Tapas').id},
     {name: 'Le Cucine Mandarosso', city_id: City.find_by(name: 'Barcelona').id , cuisine_id: Cuisine.find_by(name: 'Italian').id},
     {name: 'PerBacco!', city_id: City.find_by(name: 'Barcelona').id , cuisine_id: Cuisine.find_by(name: 'Italian').id},
-
+    {name: 'Le Baratin', city_id: City.find_by(name: 'Paris').id , cuisine_id: Cuisine.find_by(name: 'French').id},
+    {name: 'Les Arlots', city_id: City.find_by(name: 'Paris').id , cuisine_id: Cuisine.find_by(name: 'French').id},
+    {name: 'Chez Aline', city_id: City.find_by(name: 'Paris').id , cuisine_id: Cuisine.find_by(name: 'Fast Food').id},
+    {name: 'Les Philosphes', city_id: City.find_by(name: 'Paris').id , cuisine_id: Cuisine.find_by(name: 'French').id},
+    {name: 'Raviolis Chinois Nord-Est', city_id: City.find_by(name: 'Paris').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
+    {name: 'Kagari', city_id: City.find_by(name: 'Tokyo').id, cuisine_id: Cuisine.find_by(name: 'Japanese').id},
+    {name: 'RyuGin', city_id: City.find_by(name: 'Tokyo').id, cuisine_id: Cuisine.find_by(name: 'Japanese').id},
+    {name: 'Sushi Saito', city_id: City.find_by(name: 'Tokyo').id, cuisine_id: Cuisine.find_by(name: 'Japanese').id},
+    {name: '7-Eleven', city_id: City.find_by(name: 'Tokyo').id, cuisine_id: Cuisine.find_by(name: 'Fast Food').id},
+    {name: 'Nodaiwa', city_id: City.find_by(name: 'Tokyo').id, cuisine_id: Cuisine.find_by(name: 'Japanese').id},
+    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
+    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
+    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
+    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
+    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
+    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
+    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
+    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
+    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
+    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
+    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id},
+    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id},
+    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id},
+    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id},
+    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id}
   ])
