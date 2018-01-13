790af35baf1fe5ae27d06b6f4f84e1832514e45e
bkhartmire <bkhartmire@gmail.com>
2018-01-13 20:16:25 +0100

    add reviews/_form partial

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,16 @@ class ReviewsController < ApplicationController
     redirect_to restaurant_path(@review.restaurant_id)
   end
 
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to user_path(current_user)
+  end
+
   private
   def review_params
     params.require(:review).permit(:restaurant_id, :rating, :content)
