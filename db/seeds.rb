603a5c5a2ae65937943e22d129bbc995c5a99bb7
bkhartmire <bkhartmire@gmail.com>
2018-01-13 17:20:18 +0100

    finish restaurant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-cuisines = Cuisine.create([{name: 'Japanese'}, {name: 'Chinese'}, {name: 'Korean'}, {name: 'Italian'}, {name: 'French'}, {name: 'New American'}, {name: 'Mexican'}, {name: 'Fast Food'}, {name: 'Tapas'}])
+cuisines = Cuisine.create([{name: 'Japanese'}, {name: 'Chinese'}, {name: 'Korean'}, {name: 'Italian'}, {name: 'French'}, {name: 'New American'}, {name: 'Mexican'}, {name: 'Fast Food'}, {name: 'Tapas'}, {name: 'Cantonese'}])
 
-cities = City.create([{name: 'Los Angeles'}, {name: 'New York City'}, {name: 'Barcelona'}, {name: 'Paris'}, {name: 'Tokyo'}, {name: 'Hong Kong'}, {name: 'Mexico City'}, {name: 'Cape Town'}])
+cities = City.create([{name: 'Los Angeles'}, {name: 'New York City'}, {name: 'Barcelona'}, {name: 'Paris'}, {name: 'Tokyo'}, {name: 'Hong Kong'}, {name: 'Mexico City'}])
 
 restaurants = Restaurant.create([
     {name: 'Cal Mare', city_id: City.find_by(name: 'Los Angeles').id , cuisine_id: Cuisine.find_by(name: 'Italian').id},
@@ -34,19 +34,14 @@ restaurants = Restaurant.create([
     {name: 'Sushi Saito', city_id: City.find_by(name: 'Tokyo').id, cuisine_id: Cuisine.find_by(name: 'Japanese').id},
     {name: '7-Eleven', city_id: City.find_by(name: 'Tokyo').id, cuisine_id: Cuisine.find_by(name: 'Fast Food').id},
     {name: 'Nodaiwa', city_id: City.find_by(name: 'Tokyo').id, cuisine_id: Cuisine.find_by(name: 'Japanese').id},
-    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
-    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
-    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
-    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
-    {name: '', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
-    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
-    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
-    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
-    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
-    {name: '', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
-    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id},
-    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id},
-    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id},
-    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id},
-    {name: '', city_id: City.find_by(name: 'Cape Town').id, cuisine_id: Cuisine.find_by(name: '').id}
+    {name: 'Caprice', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'French').id},
+    {name: 'Lung King Heen', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Cantonese').id},
+    {name: 'Xi Yan Hong Kong', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
+    {name: 'Jumbo Kingdom', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Chinese').id},
+    {name: 'Yan Toh Heen', city_id: City.find_by(name: 'Hong Kong').id, cuisine_id: Cuisine.find_by(name: 'Cantonese').id},
+    {name: 'Pujol', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
+    {name: 'Azul Historico', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id},
+    {name: 'Suntory Del Valle', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Japanese').id},
+    {name: 'Au Pied de Cochon', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'French').id},
+    {name: 'Dulce Patria', city_id: City.find_by(name: 'Mexico City').id, cuisine_id: Cuisine.find_by(name: 'Mexican').id}
   ])
