51b592783997c73163a951fe3b7093690334744a
bkhartmire <bkhartmire@gmail.com>
2018-01-13 18:48:29 +0100

    configure omniauth with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113144414) do
+ActiveRecord::Schema.define(version: 20180113173514) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20180113144414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
