76411e501979d5ff85d1b54a3ceb6367feefc339
bkhartmire <bkhartmire@gmail.com>
2018-01-13 17:43:44 +0100

    list restaurants in city_cuisine_path

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,5 +2,8 @@ class CuisinesController < ApplicationController
   def show
     @cuisine = Cuisine.find(params[:id])
     @city = City.find(params[:city_id])
+    all_restaurants = @city.restaurants
+    @restaurants = all_restaurants.select{|restaurant| restaurant.cuisine_id == @cuisine.id}
+
   end
 end
