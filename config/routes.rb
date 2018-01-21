b2d2e4052746b66471e5627b0437f200aac78c84
bkhartmire <bkhartmire@gmail.com>
2018-01-21 16:55:55 +0100

    edit routes and views accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resources :reviews
-  resources :restaurants
+  resources :restaurants, only: [:new, :create, :destroy]
   resources :users, only: [:show]
   post 'reviews/:id/edit' => 'reviews#edit'
 end
