064ef740170fa9ccb14b4e63eed42b73c46cc519
bkhartmire <bkhartmire@gmail.com>
2018-01-21 16:50:56 +0100

    #restaurant_params

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,24 @@
 class RestaurantsController < ApplicationController
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.create(restaurant_params)
   end
 
   def edit
   end
 
   def update
-  end 
+  end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :city_id, :cuisine_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resources :reviews
-  resources :restaurant
+  resources :restaurants
   resources :users, only: [:show]
   post 'reviews/:id/edit' => 'reviews#edit'
 end
