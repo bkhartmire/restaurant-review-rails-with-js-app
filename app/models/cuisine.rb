425e446138d626a182b081e2c4e076f6f835d1e8
bkhartmire <bkhartmire@gmail.com>
2018-01-22 12:56:47 +0100

    same validation for cuisine name

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,4 +1,6 @@
 class Cuisine < ApplicationRecord
   has_many :restaurants
   has_many :cities, through: :restaurants
+
+  validates :name, format: {with: /\A[A-Z]{1}[a-z]+(\z|\s[A-Z]{1}[a-z]+)/, message: "City must be correctly capitalized and only contain alphabetical characters (e.g. Los Angeles)"}
 end
