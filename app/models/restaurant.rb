6f9011d15fdc19d6dc2bf9ba002d759dbc6e80ce
bkhartmire <bkhartmire@gmail.com>
2018-01-22 10:23:36 +0100

    custom setter/getter city cuisine restaurant methods

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,20 @@ class Restaurant < ApplicationRecord
   has_many :users, through: :reviews
   belongs_to :cuisine
   belongs_to :city
+
+  def cuisine_name=(name)
+    self.cuisine = Cuisine.find_or_create_by(name: name)
+  end
+
+  def cuisine_name
+    self.cuisine.name
+  end
+
+  def city_name=(name)
+    self.city = City.find_or_create_by(name: name)
+  end
+
+  def city_name
+    self.city.name
+  end
 end
