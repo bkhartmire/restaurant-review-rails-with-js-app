ceb754c0fe61896fae329b8e6bc85b8b3c7132e3
bkhartmire <bkhartmire@gmail.com>
2018-01-22 10:49:14 +0100

    redirects to restaurant_path

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,6 +7,7 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(@restaurant), notice: 'New listing added!'
   end
 
   def show
