9bd089c6e981a339dc6be020293e2400d62103c7
bkhartmire <bkhartmire@gmail.com>
2018-01-22 14:24:51 +0100

    finally fixed the validation errors partial :)))))

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,7 +2,7 @@ class City < ApplicationRecord
   has_many :restaurants
   has_many :cuisines, through: :restaurants
 
-  validates :name, format: {with: /\A[A-Z]{1}[a-z]+(\z|\s[A-Z]{1}[a-z]+)/, message: "City must be correctly capitalized and only contain alphabetical characters (e.g. Los Angeles)"}
+  validates :name, format: {with: /\A[A-Z]{1}[a-z]+(\z|\s[A-Z]{1}[a-z]+)/, message: "of city must be correctly capitalized and only contain alphabetical characters (e.g. Los Angeles)"}
 
   validates :name, uniqueness: true
 
