04cff91bb582791b46f05d264d65d1d9c0997132
bkhartmire <bkhartmire@gmail.com>
2018-01-22 12:52:03 +0100

    city name validation

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,6 @@
 class City < ApplicationRecord
   has_many :restaurants
   has_many :cuisines, through: :restaurants
+
+  validates :name, format: {with: /\A[A-Z]{1}[a-z]+(\z|\s[A-Z]{1}[a-z]+)/, message: "City must be correctly capitalized and only contain alphabetical characters."}
 end
