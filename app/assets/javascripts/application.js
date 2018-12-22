7277b60d2e05a9f4f8c1c047a447cc61e69d9fc7
bkhartmire <bkhartmire@gmail.com>
2018-12-22 13:03:52 -0800

    move js out of manifest file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,28 +14,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-$(document).ready(function() {
-  alert('applicaton.js is loading')
-  var link = document.getElementById('reviews_button')
-  link.addEventListener('click', function(e) {
-    listReviews(e)
-  })
-})
-
-function browseSorter() {
-  var browseValue = document.getElementById('browse_bar').value;
-  let req = $.get(browseValue)
-  req.done(function(response){
-    $("#display_list").empty().append(response)
-  })
-}
-
-function listReviews(e) {
-  e.preventDefault()
-  //why does this = the window and not the a#reviews_button element???
-  let req = $.get(document.getElementById('reviews_button').href)
-  req.done(function(response){
-    $("#reviews").empty().append(response)
-  })
-}
