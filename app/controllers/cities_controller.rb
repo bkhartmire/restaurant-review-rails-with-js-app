702c344b43d1dd797fa1ceffd5bf24c62a94dd13
bkhartmire <bkhartmire@gmail.com>
2018-12-24 14:16:38 -0800

    layout: false cities/show

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -11,6 +11,6 @@ class CitiesController < ApplicationController
 
   def show
     @city = City.find_by(id: params[:id])
-    @restaurants = @city.restaurants.sort_by{ |restaurant| restaurant.average_rating }.reverse
+    render 'cities/show', layout: false
   end
 end
