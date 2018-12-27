d4c3509e5145bad189bd08513a273ce94336c37e
bkhartmire <bkhartmire@gmail.com>
2018-12-26 19:27:09 -0800

    building out visits controller

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,2 +1,15 @@
 class VisitsController < ApplicationController
+  def new
+  end
+
+  def create
+    visit = Vist.create(visit_params)
+    redirect_to current_user
+  end
+
+  private
+
+  def visit_params
+    params.require(:visit).permit(:restaurant_id, :user_id)
+  end
 end
