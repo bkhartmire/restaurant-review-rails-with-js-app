a5e11624615cb73a4f67d8b7f30abe838955c4c2
bkhartmire <bkhartmire@gmail.com>
2018-12-26 19:41:13 -0800

    just trying things...visits#create

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,15 +1,17 @@
 class VisitsController < ApplicationController
   def new
+    @visit = Visit.new
   end
 
   def create
-    Visit.create(visit_params)
+    #debugger
+    visit = Visit.create(params[:current_user, :restaurant_id])
     redirect_to current_user
   end
 
-  private
+  #private
 
-  def visit_params
-    params.require(:visit).permit(:restaurant_id, :user_id)
-  end
+  #def visit_params
+  #  params.require(:visit).permit(:restaurant_id, :user_id)
+  #end
 end
