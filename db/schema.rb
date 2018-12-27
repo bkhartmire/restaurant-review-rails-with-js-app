df8b7c7274250f5cd058c2d7cf9dc3899ebfb800
bkhartmire <bkhartmire@gmail.com>
2018-12-26 18:59:10 -0800

    migrate and seed

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 20181227024047) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_restaurants", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,13 +74,6 @@ ActiveRecord::Schema.define(version: 20181227024047) do
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "users_restaurants", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "restaurant_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
