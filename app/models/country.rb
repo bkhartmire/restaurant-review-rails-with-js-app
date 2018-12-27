ab62f1c7a99332fde1e9fd8e6170e317f9473f6a
bkhartmire <bkhartmire@gmail.com>
2018-12-27 13:55:03 -0800

    add note to self in country.rb but don't want to change anything right now

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,6 @@
 class Country < ApplicationRecord
   has_many :cities
   has_many :restaurants, through: :cities
+  #I think you can delete this relationship? country doesn't need to have cuisines
   has_many :cuisines, through: :cities
 end
