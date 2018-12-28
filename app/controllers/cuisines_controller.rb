b17dd2a99351c5cbcc191f14aa9fb34dc8dea26c
bkhartmire <bkhartmire@gmail.com>
2018-12-28 13:35:04 -0800

    cuisines contoller's turn

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,12 +1,20 @@
 class CuisinesController < ApplicationController
   def sortAZ
     @cuisines = Cuisine.all.sort_by{|cuisine| cuisine.name}
-    render 'cuisines/index', layout: false
+    #render 'cuisines/index', layout: false
+    respond_to do |f|
+      f.html {render 'cuisines/index', layout: false}
+      f.json {render json: @cuisines}
+    end
   end
 
   def sortMost
     @cuisines = Cuisine.all.sort_by{|cuisine| cuisine.restaurants.count}.reverse
-    render 'cuisines/index', layout: false
+    #render 'cuisines/index', layout: false
+    respond_to do |f|
+      f.html {render 'cuisines/index', layout: false}
+      f.json {render json: @cuisines}
+    end
   end
 
   def show
