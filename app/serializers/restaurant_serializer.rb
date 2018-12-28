9ecb745b7500c2af480d0cb0739e9cd041ff6c59
bkhartmire <bkhartmire@gmail.com>
2018-12-28 13:13:17 -0800

    update restaurant serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,8 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :website_url
+  belongs_to :city
+  belongs_to :cuisine
+  has_many :reviews
+  has_many :visits
+  has_many :users, through: :visits
 end
