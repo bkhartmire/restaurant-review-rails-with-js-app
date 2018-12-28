6ce7c6b17b471768712c4d173650678d355f308b
bkhartmire <bkhartmire@gmail.com>
2018-12-28 13:29:49 -0800

    make cities controller resond to json

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,16 +1,28 @@
 class CitiesController < ApplicationController
   def sortAZ
     @cities = City.all.sort_by{|city| city.name}
-    render 'cities/index', layout: false
+    #render 'cities/index', layout: false
+    respond_to do |f|
+      f.html {render 'cities/index', layout: false}
+      f.json {render json: @cities}
+    end
   end
 
   def sortMost
     @cities = City.all.sort_by{|city| city.restaurants.count}.reverse
-    render 'cities/index', layout: false
+    #render 'cities/index', layout: false
+    respond_to do |f|
+      f.html {render 'cities/index', layout: false}
+      f.json {render json: @cities}
+    end
   end
 
   def show
     @city = City.find_by(id: params[:id])
-    render 'cities/show', layout: false
+    #render 'cities/show', layout: false
+    respond_to do |f|
+      f.html {render 'cities/show', layout: false}
+      f.json {render json: @city}
+    end
   end
 end
