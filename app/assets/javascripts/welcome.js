690803b607565874bfd6fe89090cdefd243533af
bkhartmire <bkhartmire@gmail.com>
2018-12-28 17:05:04 -0800

    any changes?

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  //why isn't jquery selector working? $("#browse_bar")[0] no method error
-  //($('#browse_bar')[0]).on('change', browseSorter(), false)
   if (document.getElementById('browse_bar')) {
     document.getElementById('browse_bar').addEventListener('change', () => browseSorter(), false)
   }
