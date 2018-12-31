b63aa85134d910eb0e3840af72a84406a6a82fef
bkhartmire <bkhartmire@gmail.com>
2018-12-30 19:19:54 -0800

    Delete debuggers and more files plus only render json in cities controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,25 +1,16 @@
 class CitiesController < ApplicationController
   def sortAZ
     @cities = City.all.sort_by{|city| city.name}
-    respond_to do |f|
-      f.html {render 'cities/index', layout: false}
-      f.json {render json: @cities}
-    end
+    render json: @cities
   end
 
   def sortMost
     @cities = City.all.sort_by{|city| city.restaurants.count}.reverse
-    respond_to do |f|
-      f.html {render 'cities/index', layout: false}
-      f.json {render json: @cities}
-    end
+    render json: @cities
   end
 
   def show
     @city = City.find_by(id: params[:id])
-    respond_to do |f|
-      f.html {render 'cities/show', layout: false}
-      f.json {render json: @city}
-    end
+    render json: @city
   end
 end
