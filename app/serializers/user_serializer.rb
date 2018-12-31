57dddaa269751aee0e988e50dade8b22acf26446
bkhartmire <bkhartmire@gmail.com>
2018-12-30 19:15:04 -0800

    nvm added back welcomeController

--- app/serializers/user_serializer.rb
+++
@@ -1,6 +0,0 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :admin
-  has_many :reviews
-  has_many :visits
-  has_many :restaurants, through: :visits
-end
--- app/serializers/visit_serializer.rb
+++
@@ -1,5 +0,0 @@
-class VisitSerializer < ActiveModel::Serializer
-  attributes :id
-  belongs_to :user
-  belongs_to :restaurant
-end
