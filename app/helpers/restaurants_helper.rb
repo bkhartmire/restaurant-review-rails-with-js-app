e9ecc8d005727261d1b702b34ac33e6155246b66
bkhartmire <bkhartmire@gmail.com>
2018-12-30 18:14:23 -0800

    add on more view logic to helper method

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -18,10 +18,15 @@ module RestaurantsHelper
   end
 
   def count_restaurants_greeting()
-    if current_user.restaurants.count == 1
-        raw("<h3>Congratulations! You visited your first Michelin 3-Star Restaurant!</h3>")
-    elsif current_user.restaurants.count > 1
-        raw("<h3>You've Visited #{current_user.restaurants.count} Michelin 3-Star Restaurants So Far!</h3>")
+    if current_user.restaurants.any?
+      if current_user.restaurants.count == 1
+          greeting = "<h3>Congratulations! You visited your first Michelin 3-Star Restaurant!</h3>"
+      elsif current_user.restaurants.count > 1
+          greeting = "<h3>You've Visited #{current_user.restaurants.count} Michelin 3-Star Restaurants So Far!</h3>"
+      end
+      raw("#{greeting}<h5>There are #{Restaurant.all.count - current_user.restaurants.count} more Michelin 3-Star Restaurants in the world for you to try!</h5>")
+    else
+      raw("<h3> You haven't visited any 3-Star Michelin Restaurants Yet</h3>")
     end
   end
 
