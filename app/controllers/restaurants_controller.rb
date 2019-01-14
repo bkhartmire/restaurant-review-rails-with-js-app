be6fb9ef0ba89705a3fc7f5958c5baa03a481bee
bkhartmire <bkhartmire@gmail.com>
2019-01-14 01:35:41 -0800

    delete old restaurant validations and new/create actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,20 +4,6 @@ class RestaurantsController < ApplicationController
     render json: @restaurants
   end
 
-  def new
-    @restaurant = Restaurant.new
-  end
-
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
-    if @restaurant.valid? && @restaurant.cuisine.valid? && @restaurant.city.valid?
-      @restaurant.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render :new
-    end
-  end
-
   def show
     @restaurant = Restaurant.find(params[:id])
     respond_to do |f|
@@ -26,14 +12,4 @@ class RestaurantsController < ApplicationController
     end
   end
 
-  def destroy
-    restaurant = Restaurant.find(params[:id])
-    restaurant.destroy
-    redirect_to root_path
-  end
-
-  private
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :city_name, :cuisine_name)
-  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,14 +5,6 @@ class Restaurant < ApplicationRecord
   has_many :visits
   has_many :users, through: :visits
 
-  validates :name, presence: true
-  validates :city_name, presence: true
-  validates :cuisine_name, presence: true
-
-  def self.five_stars
-    self.all.select {|restaurant| restaurant.average_rating == 5.0 }
-  end
-
   def average_rating
     if self.reviews.any?
       sum = 0
@@ -25,7 +17,4 @@ class Restaurant < ApplicationRecord
     end
   end
 
-  def country
-    self.city.country
-  end
 end
