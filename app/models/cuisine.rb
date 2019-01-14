06ee0bf7acbaac68b4f7d5b28765150b7470f0a4
bkhartmire <bkhartmire@gmail.com>
2019-01-14 07:59:09 -0800

    delete commented out code

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,6 +1,5 @@
 class Cuisine < ApplicationRecord
   has_many :restaurants
-  #has_many :cities, through: :restaurants
 
   validates :name, format: {with: /\A[A-Z]{1}[a-z]+(\z|\s[A-Z]{1}[a-z]+)/, message: "of cuisine must be correctly capitalized and only contain alphabetical characters (e.g. Fast Food)"}
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :reviews
-  #has_many :restaurants, through: :reviews
   has_many :visits
   has_many :restaurants, through: :visits
 
